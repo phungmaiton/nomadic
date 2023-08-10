e142a6a547d082936d101f76dc30aa150c89ef7c
Mai <garcia.ton94@gmail.com>
2023-08-09 20:51:38 -0400

    add button

--- src/components/UserAuth/Dashboard.js
+++ src/components/UserAuth/Dashboard.js
@@ -260,6 +260,25 @@ export default function Dashboard({
                       <div className="col-span-3">
                         <h2>Community Contributions</h2>
                       </div>
+                      <NavLink
+                        className="lg:col-span-1 text-right mb-0 lg:mb-0 lg:ml-[10%] flex justify-end items-top"
+                        to="/add-blog"
+                      >
+                        <div className="flex flex-col items-center spacy-y-1.5 relative text-xs ml-3">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            className="bi bi-plus-square"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                          </svg>
+                          <p>Add</p>
+                        </div>
+                      </NavLink>
                     </div>
                     {currentPosts && currentPosts.length > 0 ? (
                       <div className=" mt-4">
